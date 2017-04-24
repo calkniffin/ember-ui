b11ea41813f0bc8a8fbedb7c33323cd0f4a36297
Calvin Kniffin <calkniffin@gmail.com>
2017-04-23 19:29:06 -0500

    Adds loading page on routes

--- app/routes/org.js
+++ app/routes/org.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(params) {
+    return $.get(`https://api.github.com/orgs/${params.id}`).then( function(rawOrg) {
+      rawOrg.oldId = rawOrg.id;
+      rawOrg.id = rawOrg.login;
+      return rawOrg
+    }).then(function (data) {
+      return new Ember.RSVP.Promise((res, rej) => {
+        Ember.run.later(() => {
+          res(data);
+        }, 2000)
+      })
+    })
+  }
 });
--- app/routes/org/repo.js
+++ app/routes/org/repo.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(params) {
+    let orgId = Ember.get(this.modelFor('org'), 'login')
+    return $.get(`https://api.github.com/repos/${orgId}/${params.repoid}`).then(rawRepo => {
+      // backing up github's numeric ID
+      rawRepo.oldId = rawRepo.id;
+      // use the name of the repo as our ID
+      rawRepo.id = rawRepo.name;
+      console.log('repoid', params.id)
+      return rawRepo;
+    });
+  }
 });
