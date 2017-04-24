62e87e869eb79072ebbd1c71d1bc2697201cf9a6
Calvin Kniffin <calkniffin@gmail.com>
2017-04-23 19:57:15 -0500

    Handles routing errors

--- app/routes/org.js
+++ app/routes/org.js
@@ -1,10 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    error(jqxhr) {
+      if(jqxhr.status === 404) {
+        this.intermediateTransitionTo('org.notfound');
+      } else {
+        return true;
+      }
+    }
+  },
   model(params) {
     return $.get(`https://api.github.com/orgs/${params.id}`).then( function(rawOrg) {
       rawOrg.oldId = rawOrg.id;
-      rawOrg.id = rawOrg.login;
+      rawOrg.id = rawOrg.name;
       return rawOrg
     }).then(function (data) {
       return new Ember.RSVP.Promise((res, rej) => {
